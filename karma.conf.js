dab273ac8db561ef8b78ee2bab7816367c56f64a
Andrew Binstead <andrew@section9.co>
2017-09-13 14:47:13 +0100

    Made unit testing headless

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'], // Add 'Chrome' to run tests in browser
+    browsers: ['ChromeHeadless'], // Add 'Chrome' to run tests in browser
     singleRun: false
   });
 };
